41f016e2086267be33b194f676d1776b60b319f5
David Mood <davidmood@hotmail.com>
2021-09-12 23:09:54 +0430

    first commit

---
+++ firebase.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCtLzutPw1XbMaQwqjZnfVcl1TFDr51SSk",
+    authDomain: "adspace-71a3b.firebaseapp.com",
+    databaseURL: "https://adspace-71a3b.firebaseio.com",
+    projectId: "adspace-71a3b",
+    storageBucket: "adspace-71a3b.appspot.com",
+    messagingSenderId: "321439000142",
+    appId: "1:321439000142:web:65ffabe1929112e0"
+  };

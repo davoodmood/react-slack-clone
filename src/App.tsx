d8117dc14f7d71f64a9eff303b453460d3a75597
David Mood <davidmood@hotmail.com>
2021-09-13 00:39:41 +0430

    Created the Header Component and added Styled Component CSS with TypeScript.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
-// import { Counter } from './features/counter/Counter';
+// import { Counter } from './features/counter/Counter'
 import './App.css';
+import Header from './components/Header';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World! Let's build Slack.</h1>
-      </header>
+        {/* <h1>Hello World! Let's build Slack.</h1> */}
+        <Router>
+        <>
+          <Switch>
+            <Route path="/" exact>
+              {/* <Home /> */}
+              <Header />
+            </Route>
+          </Switch>
+        </>
+      </Router>      
     </div>
   );
 }
